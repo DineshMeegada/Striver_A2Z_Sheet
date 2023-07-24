db28df21436f2d06a08ae0a0a7542100231e0f57
Dinesh <dinimeegada@gmail.com>
2023-07-24 15:39:23 +0530

    committing 3rd in 5.2

---
+++ 5. Strings [Basic & Medium]/5.2 Medium String Problems/3_RomanToInt.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int romanToInt(string roman){
+    
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    string roman;
+    cin >> roman;
+
+    int integer = romanToInt(roman);
+    cout << integer;
+    
+    return 0;
+}
