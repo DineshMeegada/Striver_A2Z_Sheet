cc353e8fc727238675eaf78423d469c1e3916acc
Dinesh <dinimeegada@gmail.com>
2023-07-25 07:32:33 +0530

    committing 3rd in 5.2

--- 5. Strings [Basic & Medium]/5.2 Medium String Problems/3_RomanToInt.cpp
+++ 5. Strings [Basic & Medium]/5.2 Medium String Problems/3_RomanToInt.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int romanToInt(string roman){
-    
+    unordered_map <char, int> symbols = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int n = roman.size();
+    int num = 0;
+
+    for (int i=0; i<n; i++) {
+        if (i<n-1 && symbols[roman[i+1]]>symbols[roman[i]]) num -= symbols[roman[i]];
+        else num += symbols[roman[i]];
+    }
+    return num;
 }
 
 int main() {
