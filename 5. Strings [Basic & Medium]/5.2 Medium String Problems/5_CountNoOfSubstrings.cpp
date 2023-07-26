a09cbda22010a6cbf30868b17cffcf6864c88cf4
Dinesh <dinimeegada@gmail.com>
2023-07-27 03:07:06 +0530

    committing 5th in 5.2

--- 5. Strings [Basic & Medium]/5.2 Medium String Problems/5_CountNoOfSubstrings.cpp
+++ 5. Strings [Basic & Medium]/5.2 Medium String Problems/5_CountNoOfSubstrings.cpp
@@ -1,8 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int countUptoK(string str, int k) {
+    unordered_map<char, int> cnt;
+    int left = 0, res = 0;
+    for (int i=0; i<str.size(); i++) {
+        cnt[str[i]]++;
+        while (cnt.size() > k) {
+            cnt[str[left]]--;
+            if (cnt[str[left++]] == 0) cnt.erase(str[left]);
+        }
+
+        res += i-left+1;
+    }
+
+    return res;
+}
+
+int countOfSubstringsN(string str, int k) {
+    return (countUptoK(str, k) - countUptoK(str, k-1));
+}
+
 int countOfSubstringsN2(string str, int k) {
-    
+    int res = 0, n = str.size();
+    for (int i=0; i<n; i++) {
+        unordered_map<char, int> cnt;
+        for (int j=i; j<n; j++) {
+            cnt[str[j]]++;
+            if (cnt.size() == k) res++;
+            if (cnt.size()>k) break;
+        }
+    }
+
+    return res;
 }
 
 // Time Complexity - O(N^3)
@@ -31,7 +61,7 @@ int main() {
     int k;
     cin >> k;
 
-    int count = countOfSubstrings(str, k);
+    int count = countOfSubstringsN2(str, k);
     cout << count;
     
     return 0;
