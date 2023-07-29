1cb429e57bf7dfbdef936004cfc80efe22eb0233
Dinesh <dinimeegada@gmail.com>
2023-07-29 16:46:33 +0530

    Deletion of a node in LL

--- 6. Learn Linked Lists/6.1 Learn 1D Linked List/3_DeleteNodeInLL.cpp
+++ 6. Learn Linked Lists/6.1 Learn 1D Linked List/3_DeleteNodeInLL.cpp
@@ -1,25 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// class Node {
-//     public :
-//     int data;
-//     Node* next;
-
-//     Node(int data){
-//         this -> data = data;
-//         this -> next = NULL;
-//     }
-// };
-
 struct Node
 {
-    /* data */
     int data;
     Node* next;
-    Node(int val) : data(val), next(nullptr) {}
+    Node(int data): data(data), next(nullptr) {}
 };
-
 
 void insertAtTail(Node* &tail, int d){
     Node* temp = new Node(d);
@@ -29,7 +16,7 @@ void insertAtTail(Node* &tail, int d){
 
 Node* constructLL(vector<int>& arr) {
     // Write your code here
-    if (arr.size() == 0) return NULL;
+    if (arr.size() == 0) return nullptr;
 
     Node* head = new Node(arr[0]);
     Node* curr = head;
@@ -41,13 +28,28 @@ Node* constructLL(vector<int>& arr) {
     return head;
 }
 
-void printLL (Node* &head){
-    Node* temp = head;
+void deleteNode(Node* head, int position){
+    if (head == nullptr) return;
+    Node* prev = nullptr;
+    Node* curr = head;
 
-    while (temp != NULL) {
-        cout << temp->data << " " ;
-        temp = temp->next;
+    while (curr!=nullptr && --position) {
+        prev = curr;
+        curr = curr->next;
     }
+
+    if (curr == nullptr) return;
+
+    prev->next = curr->next;
+    delete curr;
+}
+
+void printLL (Node* head){
+    while (head!=nullptr){
+        cout << head->data << " " ;
+        head = head->next;
+    }
+    cout << endl;
 }
 
 int main() {
@@ -63,6 +65,10 @@ int main() {
     
     Node* head = constructLL(arr);
     printLL(head);
+
+    deleteNode(head, 3);
+    printLL(head);
+
 
     
     return 0;
