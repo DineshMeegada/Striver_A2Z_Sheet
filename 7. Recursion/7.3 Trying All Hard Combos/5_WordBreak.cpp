306c3e37176e52cd0d14ac06ad325da9f97d1fbc
Dinesh <dinimeegada@gmail.com>
2023-08-24 13:11:48 +0530

    word break

--- 7. Recursion/7.3 Trying All Hard Combos/5_WordBreak.cpp
+++ 7. Recursion/7.3 Trying All Hard Combos/5_WordBreak.cpp
@@ -1,27 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool solve(int ind, string s, vector<string> &words) {
-    if (ind >= s.size()) return true;
+// Backtracking Partially accepted
 
-    for (auto it: words){
-        if (s[ind] == it[0]){
-            int k = 0, i=ind;
-            while (k<it.size() && s[i]==it[k]){
-                k++;
-                i++;
+// bool solve(int ind, string s, vector<string> &words) {
+//     if (ind >= n) return true;
+
+//     for (auto it: words){
+//         if (s[ind] == it[0]){
+//             int k = 0, i=ind;
+//             while (k<it.size() && s[i]==it[k]){
+//                 k++;
+//                 i++;
+//             }
+
+//             if (k!=it.size()) continue;
+//             if (solve(i, s, words)) return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+// bool wordBreak(string s, vector<string>& wordDict) {
+//     return solve(0, s, wordDict);
+// }
+
+
+// DP Approach (Optimized)
+
+bool wordBreak(string s, vector<string>& wordDict) {
+    int n = s.size();
+
+    vector<bool> dp (n+1, false);
+    dp[n] = true;
+
+    for (int i=n-1; i>=0; i--) {
+        for (auto w: wordDict){
+            int w_len = w.size();
+            if (i+w_len<=n && s.substr(i, w_len)==w){
+                dp[i] = dp[i + w_len];
             }
 
-            if (k!=it.size()) continue;
-            if (solve(i, s, words)) return true;
+            if (dp[i]) break;
         }
     }
 
-    return false;
-}
-
-bool wordBreak(string s, vector<string>& wordDict) {
-    return solve(0, s, wordDict);
+    return dp[0];
 }
 
 int main() {
