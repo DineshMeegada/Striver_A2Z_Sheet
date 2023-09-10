0c5dabae7b404fe0bbf625aea0510080a600839a
Dinesh <dinimeegada@gmail.com>
2023-09-10 06:31:28 +0530

    isHeap

--- 11. Heaps/11.1 Learning/3_CheckIfAnArrayRepresentsMinHeap.cpp
+++ 11. Heaps/11.1 Learning/3_CheckIfAnArrayRepresentsMinHeap.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool isHeap(vector<int> arr, int n) {
     for (int i=0; i<n/2; i++) {
         int child1 = 2*i+1, child2 = 2*i+2;
-        if (arr[child1]<arr[i] || arr[child2]<arr[i]) return false;
+        if (arr[child1]<arr[i] || (child2<n && arr[child2]<arr[i])) return false;
     }
 
     return true;
