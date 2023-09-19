b62f55947e4daa1a0c5819653e1e1ec8516b35ee
Dinesh <dinimeegada@gmail.com>
2023-09-19 13:28:30 +0530

    Height of Tree

---
+++ 13. Binary Trees/Medium Problems/1_HeightOfTree.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+// Using Recursion
+int maxDepthR(TreeNode* root) {
+    if (!root) return 0;
+
+    int lh = maxDepthR(root->left);
+    int rh = maxDepthR(root->right);
+
+    return 1 + max(lh, rh);
+}
+
+// Using Queue
+int maxDepthQ(TreeNode* root) {
+    int height = 0;
+    if (!root) return height;
+
+    queue<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        height++;
+        int n = st.size();
+        for (int i=0; i<n; i++) {
+            TreeNode *curr = st.front();
+            st.pop();
+
+            if (curr->left) st.push(curr->left);
+            if (curr->right) st.push(curr->right);
+        }
+    }
+
+    return height;
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for (int i=0; i<n; i++) cin >> arr[i] ;
+    
+    TreeNode *root = createTree(arr);
+    int height = maxDepthR(root);
+    cout << height;
+    
+    
+    
+    return 0;
+}
