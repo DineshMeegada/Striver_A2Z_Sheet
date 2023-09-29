fe5b6f7cff1bec4be8152a867edbe2a1b70ee269
Dinesh <dinimeegada@gmail.com>
2023-09-29 23:55:30 +0530

    Left & Right Views of BT

---
+++ 13. Binary Trees/Medium Problems/11_Left&RightSideViewsOfBT.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+
+
+// Right Side View
+class Solution {
+public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()==level) res.push_back(root->data);
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+    }
+    
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+    }
+};
+
+
+// Left Side View
+class Solution {
+public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL) return ;
+        if(res.size()==level) res.push_back(root->data);
+        recursion(root->left, level+1, res);
+        recursion(root->right, level+1, res);
+    }
+    
+    vector<int> leftSideView(TreeNode *root) {
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+    }
+};
