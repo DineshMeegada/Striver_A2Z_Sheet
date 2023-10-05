b759ed92f0ac43881003c0585d9e4be9591c69d1
Dinesh <dinimeegada@gmail.com>
2023-10-05 14:58:17 +0530

    ceil in BST

---
+++ 14. Binary Search Trees/14.2 Practice Problems/1_CeilInBST.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+int findCeil(TreeNode *node, int x){
+    int val = -1;
+
+    while (node) {
+        if (node->data == x) return x;
+        if (node->data < x) node = node->right;
+        else {
+            val = node->data;
+            node = node->left;
+        }
+    }
+
+    return val;
+
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+    
+    return 0;
+}
