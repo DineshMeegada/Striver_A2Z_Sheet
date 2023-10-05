3e26b50ebc7e36605d7f8ed0452221983835e1a4
Dinesh <dinimeegada@gmail.com>
2023-10-05 13:36:41 +0530

    Binary Search Tree

---
+++ 14. Binary Search Trees/14.1 Concepts/1_BinarySearchTrees.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+bool isValidBST(vector<int> &order){
+    int n = order.size();
+    for (int i = 0; i < n-1; i++) {
+        if (order[i] >= order[i+1]) return false;
+    }
+
+    return true;
+}
+
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for (int i=0; i<n; i++) cin >> arr[i] ;
+    
+    TreeNode *root = createTree(arr);
+    cout << isValidBST(arr);
+    
+    
+    return 0;
+}
