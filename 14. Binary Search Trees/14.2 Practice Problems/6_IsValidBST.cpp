0d023acb651826b33f1e6d4b9cf499c6237419b1
Dinesh <dinimeegada@gmail.com>
2023-10-06 04:46:26 +0530

    Is Valid BST

---
+++ 14. Binary Search Trees/14.2 Practice Problems/6_IsValidBST.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+bool helper(TreeNode *root, long low, long high) {
+    if (!root) return true;
+    if (root->data <= low || root->data >= high) return false;
+
+    return helper(root->left, low, root->data) && helper(root->right, root->data, high);
+}
+
+bool isdataidBST(TreeNode* root) {
+    if (!root) return true;
+    return helper(root, LONG_MIN, LONG_MAX);
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+    
+    
+    return 0;
+}
