856ef626669499cd0936170957df0589da8ab806
Dinesh <dinimeegada@gmail.com>
2023-10-05 14:48:29 +0530

    Min Element in BST

---
+++ 14. Binary Search Trees/14.1 Concepts/3_MinEleInBST.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+int minVal(TreeNode* root){
+	if (!root) return -1;
+
+	while (root->left) {
+		root = root->left;
+	}
+
+	return root->data;
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+    
+    
+    return 0;
+}
