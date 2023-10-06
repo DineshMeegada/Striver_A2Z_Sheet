820170005afe2a875ebfe688155da71b68e2a58a
Dinesh <dinimeegada@gmail.com>
2023-10-06 10:11:21 +0530

    Construct BST From Preorder

---
+++ 14. Binary Search Trees/14.2 Practice Problems/8_ConstructBSTFromPreOrder.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : data(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
+TreeNode* formTree(vector<int> &arr, int n, int i){
+    TreeNode* curr = new TreeNode(arr[i]);
+
+    int l = 2*i+1, r = 2*i+2;
+    if (l<n) curr->left = formTree(arr, n, l);
+    if (r<n) curr->right = formTree(arr, n, r);
+
+    return curr;
+}
+
+TreeNode* createTree(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    return formTree(arr, arr.size(), 0);
+}
+
+TreeNode* helper(vector<int> arr, int &i, int bound){
+    if(i==arr.size() || arr[i]>bound) return NULL;
+
+    TreeNode *root = new TreeNode (arr[i++]);
+    root->left = helper(arr, i, root->data);
+    root->right = helper(arr, i, bound);
+
+    return root;
+}
+
+TreeNode* bstFromPreorder(vector<int>& preorder) {
+    int i = 0;
+    return helper(preorder, i, INT_MAX);
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+    
+    
+    return 0;
+}
