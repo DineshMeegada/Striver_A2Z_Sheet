33090467e9ac2524664ce1c949c7e0e1ac1d9d44
Dinesh <dinimeegada@gmail.com>
2023-10-13 15:21:42 +0530

    Surrounded Region DFS

---
+++ 15. Graphs/15.2 Problems on BFS & DFS/8_SurroundedRegions.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int row, int col, vector<vector<char>> &board, vector<vector<char>> &res){
+    int n = board.size(), m = board[0].size();
+    if (row<0 || row>=n || col<0 || col>=m || board[row][col]!='O' || res[row][col]=='O') return;
+
+    res[row][col] = 'O';
+
+    vector<int> rowNums = {-1, 0, 1, 0};
+    vector<int> colNums = {0, -1, 0, 1};
+
+    for (int i=0; i<4; i++) {
+        int nrow = row + rowNums[i];
+        int ncol = col + colNums[i];
+
+        dfs(nrow, ncol, board, res);
+    }
+    
+}
+
+vector<vector<char>> fill(vector<vector<char>> board) {
+    int n = board.size(), m = board[0].size();
+    vector<vector<char>> res(n, vector<char>(m, 'X'));
+
+    for (int i=0; i<n; i++) {
+        if (board[i][0]=='O' && res[i][0]=='X') dfs(i, 0, board, res);
+        if (board[i][m-1]=='O' && res[i][m-1]=='X') dfs(i, m-1, board, res);
+    }
+
+    return res;
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> board(n, vector<char> (m));
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) cin >> board[i][j];
+    }
+
+    vector<vector<char>> res = fill(board);
+    for (auto row: res) {
+        for (auto it: row) cout << it << ' ';
+        cout << endl;
+    }
+
+
+    
+    
+    
+    return 0;
+}
