4b0cd0f76a7235e8ab99b678d8a45821c4e108b1
Dinesh <dinimeegada@gmail.com>
2023-10-16 22:57:00 +0530

    Eventually Safe Nodes BFS

--- 15. Graphs/15.3 TopoSort & Problems/6_EventuallySafeNodesDFS.cpp
+++ 15. Graphs/15.3 TopoSort & Problems/6_EventuallySafeNodesDFS.cpp
@@ -17,6 +17,7 @@ vector<int> &check) {
     return false;
 }
 
+//  Using DFS of finding cycle in DAG
 vector<int> eventualSafeNodes(int v, vector<int> adj[]) {
     vector<int> vis(v, 0), path(v, 0), check(v, 0);
     vector<int> safeNodes;
@@ -30,6 +31,46 @@ vector<int> eventualSafeNodes(int v, vector<int> adj[]) {
     }
 
     return safeNodes;
+}
+
+
+// Using Topological Sort BFS
+vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    int n = graph.size();
+    vector<int> adjRev[n];
+    for (int i=0; i<n; i++) {
+        for (auto it: graph[i]){
+            adjRev[it].push_back(i);
+        }
+    }
+
+    vector<int> inDegree(n);
+    for (int i=0; i<n; i++) {
+        for (auto it: adjRev[i]) inDegree[it]++;
+    }
+
+    queue<int> q;
+    for (int i=0; i<n; i++) {
+        if (inDegree[i]==0) q.push(i);
+        if (inDegree[i]==0) cout << i << ' ';
+    }
+
+    vector<int> res;
+    while (!q.empty()) {
+        int node = q.front();
+        res.push_back(node);
+        q.pop();
+
+        for (auto it: adjRev[node]){
+            inDegree[it]--;
+            if(inDegree[it]==0) {
+                q.push(it);
+            } 
+        }
+    }
+
+    sort(res.begin(), res.end());
+    return res;
 }
 
 int main() {
