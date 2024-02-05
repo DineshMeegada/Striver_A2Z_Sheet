f3f16a73e75e42e6c924118f53e517fe5bef0704
Dinesh <dinimeegada@gmail.com>
2024-02-06 02:07:14 +0530

    Min Insertions or Deletions to convert string

---
+++ 16. DP/16.5 DP on Strings/6_MinInsertionsToConvertString.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tabulation
+int findLCS(string& s1, string& s2) {
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+            else
+                dp[ind1][ind2] =
+                    max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+int minDistance(string word1, string word2) {
+    int len = findLCS(word1, word2);
+    return word1.size() + word2.size() - 2 * len;
+}
+
+// Space Optimization
+int findLCS(string& s1, string& s2) {
+    int n = s1.size(), m = s2.size();
+    // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    vector<int> prev(m+1, 0), cur(m+1, 0);
+
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                cur[ind2] = 1 + prev[ind2 - 1];
+            else
+                cur[ind2] =
+                    max(prev[ind2], cur[ind2 - 1]);
+        }
+
+        prev = cur;
+    }
+
+    return prev[m];
+}
+
+int minDistance(string word1, string word2) {
+    int len = findLCS(word1, word2);
+    return word1.size() + word2.size() - 2 * len;
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+    
+    
+    return 0;
+}
