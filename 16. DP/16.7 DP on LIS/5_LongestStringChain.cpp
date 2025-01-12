e131e5ba8419ebdcd0b3d231b5510717d9bc82b1
DineshMeegada <dinimeegada@gmail.com>
2025-01-12 09:13:21 +0530

    Longest String Chain Updated

--- 16. DP/16.7 DP on LIS/5_LongestStringChain.cpp
+++ 16. DP/16.7 DP on LIS/5_LongestStringChain.cpp
@@ -19,6 +19,12 @@ bool compare(string &s1, string &s2)
     return i == n && j == m;
 }
 
+static bool comp(string &word1, string &word2)
+{
+    return word1.size() < word2.size();
+}
+
+// Memoization Approach
 int solve(int ind, int prev, vector<string> &words, vector<vector<int>> &dp)
 {
     int n = words.size();
@@ -35,18 +41,62 @@ int solve(int ind, int prev, vector<string> &words, vector<vector<int>> &dp)
     return dp[ind][prev + 1] = max(take, notTake);
 }
 
-static bool comp(string &word1, string &word2)
-{
-    return word1.size() < word2.size();
-}
-
-int longestStrChain(vector<string> &words)
+int longestStrChain1(vector<string> &words)
 {
     int n = words.size();
     sort(words.begin(), words.end(), comp);
 
     vector<vector<int>> dp(n, vector<int>(n + 1, -1));
     int ans = solve(0, -1, words, dp);
+    return ans;
+}
+
+// Tabulation Approach
+int longestStrChain2(vector<string> &words)
+{
+    int n = words.size();
+    sort(words.begin(), words.end(), comp);
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    for (int ind = n - 1; ind >= 0; ind--)
+    {
+        for (int prev = ind - 1; prev >= -1; prev--)
+        {
+            int notTake = dp[ind + 1][prev + 1];
+            int take = 0;
+            if (prev == -1 || compare(words[ind], words[prev]))
+                take = 1 + dp[ind + 1][ind + 1];
+
+            dp[ind][prev + 1] = max(take, notTake);
+        }
+    }
+
+    int ans = dp[0][0];
+    return ans;
+}
+
+// Space Optimization
+int longestStrChain(vector<string> &words)
+{
+    int n = words.size();
+    sort(words.begin(), words.end(), comp);
+    vector<int> cur(n + 1, 0), forw(n + 1, 0);
+
+    for (int ind = n - 1; ind >= 0; ind--)
+    {
+        for (int prev = ind - 1; prev >= -1; prev--)
+        {
+            int notTake = forw[prev + 1];
+            int take = 0;
+            if (prev == -1 || compare(words[ind], words[prev]))
+                take = 1 + forw[ind + 1];
+
+            cur[prev + 1] = max(take, notTake);
+        }
+        forw = cur;
+    }
+
+    int ans = cur[0];
     return ans;
 }
 
